575511bc87876e27d4926567211ed2ac4c273e6b
Jonathan Brosnan <Jonathan.brosnan28@gmail.com>
2023-08-29 21:17:45 -0500

    Update

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -4,11 +4,41 @@
 //isBlackAndWhite
 //Operations: Add, edit, view, delete
 
-string title = "", genre = "", desc = "", rating = "";
+string title = "", genre = "", description = "", rating = "";
 
-int length = 0, releaseYear = 1900, budget = 0;
+int length = 0, releaseYear = 1900;
+    
+decimal budget = 125.45M;
 
 bool isBlackAndWhite = false;
 
+
+Console.WriteLine("Enter a title: ");
+title = Console.ReadLine();
+
+Console.WriteLine("Enter a description: ");
+description = Console.ReadLine();
+
+
+//TODO: Fix length
+Console.WriteLine("Enter the run length in minutes: ");
+string lengthString = Console.ReadLine();
 Console.WriteLine(title);
-Console.WriteLine(length);
+Console.WriteLine(description);
+Console.WriteLine(lengthString);
+
+
+/*
+double ex1 = 3.14159;
+
+char letterGrade = 'A';
+
+{
+    int hours = 5;
+    title = "Jaws";
+    Console.WriteLine(title);
+    Console.WriteLine(length);
+
+
+}
+*/
