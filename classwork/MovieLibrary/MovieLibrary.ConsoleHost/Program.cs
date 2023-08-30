84d460e81302c7d9c97103c43dcccf3842d9af39
Jonathan Brosnan <Jonathan.brosnan28@gmail.com>
2023-08-29 20:35:50 -0500

    Update

---
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,14 @@
+﻿//Get MOVIE
+//Title,genre, decription,rating
+// Length, release year, budget
+//isBlackAndWhite
+//Operations: Add, edit, view, delete
+
+string title = "", genre = "", desc = "", rating = "";
+
+int length = 0, releaseYear = 1900, budget = 0;
+
+bool isBlackAndWhite = false;
+
+Console.WriteLine(title);
+Console.WriteLine(length);
