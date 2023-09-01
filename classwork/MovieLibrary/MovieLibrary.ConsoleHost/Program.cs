e01d76c0307dfedaf6198ac28723b7bd257c8c26
Jonathan Brosnan <Jonathan.brosnan28@gmail.com>
2023-08-31 21:39:41 -0500

    Latest

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -7,26 +7,56 @@
 string title = "", genre = "", description = "", rating = "";
 
 int length = 0, releaseYear = 1900;
-    
+
 decimal budget = 125.45M;
 
 bool isBlackAndWhite = false;
 
 
-Console.WriteLine("Enter a title: ");
-title = Console.ReadLine();
-
-Console.WriteLine("Enter a description: ");
-description = Console.ReadLine();
+getMovie();
+DisplayMovie();
 
 
-//TODO: Fix length
-Console.WriteLine("Enter the run length in minutes: ");
-string lengthString = Console.ReadLine();
-Console.WriteLine(title);
-Console.WriteLine(description);
-Console.WriteLine(lengthString);
+//Functions
+void getMovie ()
+{
+    title = ReadString("Enter a title: ",true);
+    description = ReadString("Enter a description: ",false);
+    //TODO: Fix length
+    length = ReadInt("Enter the run length in minutes: ",0);
+    releaseYear = ReadInt("Enter the release year:", 1900);
+}
 
+void DisplayMovie ()
+{
+    Console.WriteLine(title);
+    Console.WriteLine(description);
+    Console.WriteLine(length);
+}
+
+int ReadInt (string message, int minimumValue)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    
+    int result;
+    if (Int32.TryParse(value,out result))
+        if (result >= minimumValue)
+            return result;
+    
+    return 0;
+}
+string ReadString (string message, bool isRequired)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+
+    if (!isRequired)
+        return value;
+
+
+    return value;
+}
 
 /*
 double ex1 = 3.14159;
