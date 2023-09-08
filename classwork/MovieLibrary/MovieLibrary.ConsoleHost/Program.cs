9096a1e912c3d47e7635dad20c5e03e7b9c0899f
Jonathan Brosnan <Jonathan.brosnan28@gmail.com>
2023-09-07 21:34:53 -0500

    String Intro

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -13,12 +13,46 @@ decimal budget = 125.45M;
 bool isBlackAndWhite = false;
 
 
-getMovie();
-DisplayMovie();
+//Entry Point
+var done = false;
+do
+{
+    //var command = DisplayMenu();
+
+    switch (DisplayMenu())
+    {
+        case 1: AddMovie();
+            break;
+        case 2: EditMovie();
+            break;
+        case 3: DeleteMovie();
+            break;
+        case 4: ViewMovie();
+            break;
+        case 0: done = true;
+            break;
+        default: Console.WriteLine("Unknown Option");
+            break;
+
+    };/*
+    if (command == 1)
+        AddMovie();
+    else if (command == 2)
+        EditMovie();
+    else if (command == 3)
+        DeleteMovie();
+    else if (command == 4)
+        ViewMovie();
+    else if (command == 0)
+        done = true;
+    */
+} while (!done);
+
+
 
 
 //Functions
-void getMovie ()
+void AddMovie ()
 {
     title = ReadString("Enter a title: ",true);
     description = ReadString("Enter a description: ",false);
@@ -27,8 +61,24 @@ void getMovie ()
     releaseYear = ReadInt("Enter the release year:", 1900);
 
     genre = ReadString("Enter the genre: ",false);
-    rating = ReadString("Enter the rating: ", false);
+    rating = ReadRating("Enter the rating: ");
     isBlackAndWhite = ReadBoolean("Black and White (Y/N): ");
+}
+
+void EditMovie()
+{
+    Console.WriteLine("Not Imp.");
+}
+void DeleteMovie ()
+{
+    if (!Confirm("Are you sure you want to delete the movie?(Y/N)"))
+        return;
+    Console.WriteLine("Movie Deleted!");
+}
+
+bool Confirm(string message)
+{
+    return ReadBoolean(message);
 }
 
 bool ReadBoolean (string message)
@@ -37,19 +87,60 @@ bool ReadBoolean (string message)
 
     while (true)
     {
-        var value = Console.ReadLine();
-        if (value == "Y" || value == "y")
-            return true;
-        else if (value == "N" || value == "n")
-            return false;
+   
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.Y: return true;
+            case ConsoleKey.N: return false;
+        };
 
-        Console.WriteLine("Please enter Y/N");
+        //Console.WriteLine("Please enter Y/N");
     };
  
     
 }
 
-void DisplayMovie ()
+int DisplayMenu ()
+{
+    Console.WriteLine("----------------");
+    Console.WriteLine("A)dd Movie");
+    Console.WriteLine("E)dit Movie");
+    Console.WriteLine("D)elete Movie");
+    Console.WriteLine("V)iew Movie");
+    Console.WriteLine("Q)uit");
+
+    do
+    {   /*
+        var input = Console.ReadLine();
+        if (input == "A" || input == "a")
+            return 1;
+        else if (input == "E" || input == "e")
+            return 2;
+        else if (input == "D" || input == "d")
+            return 3;
+        else if (input == "V" || input == "v")
+            return 4;
+        else if (input == "Q" || input == "q")
+            return 0;
+        */
+
+        switch (Console.ReadKey(true).Key)
+        {
+           
+            case ConsoleKey.A: return 1;
+            case ConsoleKey.E: return 2;
+            case ConsoleKey.D: return 3;
+            case ConsoleKey.V: return 4;
+            case ConsoleKey.Q: return 0;
+           
+           
+
+
+        };
+    }while(true);
+}
+
+void ViewMovie ()
 {
 
     Console.WriteLine("\n------------------------");
@@ -71,7 +162,7 @@ void DisplayMovie ()
         Console.WriteLine("The movie is not black and white.");
     }
   */
-
+    //filepath = @"C:\windows\temp"
 }
 
 int ReadInt (string message, int minimumValue)
@@ -97,7 +188,7 @@ string ReadString (string message, bool isRequired)
     {
         string value = Console.ReadLine();
 
-        if (!isRequired || value != "")
+        if (!isRequired || !String.IsNullOrEmpty(value))
             return value;
 
 
@@ -106,6 +197,29 @@ string ReadString (string message, bool isRequired)
    
 }
 
+
+string ReadRating ( string message )
+{
+    Console.WriteLine(message);
+    do
+    {
+        string value = Console.ReadLine();
+
+        if (value == "PG")
+            return "PG";
+        else if (value == "G")
+            return "G";
+        else if (value == "PG-13")
+            return "PG-13";
+        else if (value == "R")
+            return "R";
+        else if (String.IsNullOrEmpty(value)) //String.Empty //String.IsNullOrEmpty()
+            return "";
+
+        Console.WriteLine("Invalid Rating");
+    } while (true);
+
+}
 /*
 double ex1 = 3.14159;
 
