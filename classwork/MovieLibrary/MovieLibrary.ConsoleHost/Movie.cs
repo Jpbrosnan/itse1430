b50bc6c94ebcd80707ad04989e37ef0a5fc5f131
Jonathan Brosnan <Jonathan.brosnan28@gmail.com>
2023-09-21 20:23:40 -0500

    Methods and Properties

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Movie.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Movie.cs
@@ -6,55 +6,120 @@ namespace MovieLibrary
 {
     /// <summary>Represents a movie.</summary>
     /// <remarks>
-    /// Paragraphs of descriptions.
+    /// Additional remarks to help when using the class.
     /// </remarks>
     public class Movie
     {
         //Fields - data
         /// <summary>Title of movie.</summary>
-        public string title = "";
-        public string description = "";
-        public string genre = "";
-        public string rating = "";
+        private string title = "";
+        private string description = "";
+        private string genre = "";
+        private string rating = "";
 
-        public int length;
-        public int releaseYear = 1900;
-        public bool isBlackAndWhite;
+        private int length;
+        private int releaseYear = 1900;
+        private bool isBlackAndWhite;
 
-        /// <summary>
-        /// Download metadata from internet.
-        /// </summary>
-        /// <remarks>
-        /// Additional Info Large Amount.
-        /// </remarks>
-        void DownloadMetadata ()
+        //Properties - data with functionality
+
+        public string Title
         {
-
+            get { return title; }
+            set {
+                title = value;
+            }
         }
-        /// <summary>
-        /// Validates the movie instance.
-        /// </summary>
-        /// <returns>
-        /// Error message if invalid or empty string if valid
-        /// </returns>
-        public string Validate ()
+        public string Description
+        {
+            get { return description; }
+            set {
+                description=value;
+            }
+        }
+        public string Genre
+        {
+            get {
+                return genre;
+            }
+            set {
+                genre=value;
+            }
+        }
+        public string Rating
+        {
+            get {
+                return rating;
+            }
+            set {
+                rating=value;
+            }
+        }
+        public int Length
+        {
+            get {
+                return length;
+            }
+            set {
+                length=value;
+            }
+        }
+        public int ReleaseYear
+        {
+            get {
+                return releaseYear;
+            }
+            set {
+                releaseYear=value;
+            }
+        }
+        public bool IsBlackAndWhite
+        {
+            get {
+                return isBlackAndWhite;
+            }
+            set {
+                isBlackAndWhite=value;
+            }
+        }
+
+        //Methods - functionality
+
+        /// <summary>Download metadata from Internet.</summary>
+        /// <remarks>
+        /// Searches IMDB and TheTVDB.com.
+        /// </remarks>
+        private void DownloadMetadata ()
+        { }
+
+        /// <summary>Validates the movie instance.</summary>
+        /// <returns>Error message if invalid or empty string otherwise.</returns>
+        public string Validate () /* Movie this */
         {
             //Title is required
-            //REelease Year >= 1900
-            //Length >= 0
-            //Rating is in the list
-            //If release year < 1940 than isblackandwhite must be true
+            //if (String.IsNullOrEmpty(this.title))
             if (String.IsNullOrEmpty(title))
-                return "Title is required!";
-            if (releaseYear < 1900)
-                return "release year must be greater than 1900";
-            if (length < 0)
-                return "length must be greater than or equal to 0";
-            if (releaseYear < 1940 && !isBlackAndWhite)
+                return "Title is required";
+
+            //var releaseYear = 10;
+
+            //Release Year >= 1900
+            //if (this.releaseYear < 1900)
+            if (ReleaseYear < 1900)
+                return "Release Year must be >= 1900";
+
+            //Length >= 0
+            if (Length < 0)
+                return "Length must be at least 0";
+
+            //TODO: Rating is in a list
+
+            //If ReleaseYear < 1940 then IsBlackAndWhite must be true
+            if (ReleaseYear < 1940 && !IsBlackAndWhite)
                 return "Movies before 1940 must be black and white";
 
+            //Valid
             return "";
         }
-
     }
 }
